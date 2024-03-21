cd4381ee465462b1eb26f0f08f25f1a8713e6b91
phiratio <phiratio161@gmail.com>
2024-03-21 12:44:25 +0200

    refactor: add additional comments

--- variables.tf
+++ variables.tf
@@ -6,9 +6,11 @@ variable "organization_name" {
 variable "github_app_installation_id" {
   default = "48009653"
   type    = number
+  description = "you can get the installation id from the github app settings -> installations -> see the URL"
 }
 
 variable "github_organization_name" {
   default = "phiratio"
   type    = string
+  description = "github organization/username"
 }
