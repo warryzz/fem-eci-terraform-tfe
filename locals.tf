bcd4f92f9000da1a538a57162d83f49b82915ddc
phiratio <phiratio161@gmail.com>
2024-03-21 12:59:16 +0200

    style: apply tf fmt

--- locals.tf
+++ locals.tf
@@ -6,10 +6,10 @@ locals {
   }
   workspace = {
     "fem-eci-tfe" = {
-      description    = "This is a workspace for the ECI"
-      execution_mode = "remote"
-      project_id     = module.project["fem-eci-project"].id
-      vcs_repo_identifier = "phiratio/fem-eci-terraform-tfe"
+      description         = "This is a workspace for the ECI"
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
     }
     "fem-eci-github" = {
       description         = "This is a workspace for GitHub resources."
--- variables.tf
+++ variables.tf
@@ -4,13 +4,13 @@ variable "organization_name" {
 }
 
 variable "github_app_installation_id" {
-  default = "48009653"
-  type    = number
+  default     = "48009653"
+  type        = number
   description = "you can get the installation id from the github app settings -> installations -> see the URL"
 }
 
 variable "github_organization_name" {
-  default = "phiratio"
-  type    = string
+  default     = "phiratio"
+  type        = string
   description = "github organization/username"
 }
